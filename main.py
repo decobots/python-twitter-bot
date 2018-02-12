76c350fa2717039a5a915a9171f1216690558ff3
decobots <deco-bots@yandex.ru>
2018-02-12 14:14:37 +0300

    all functions updated

--- main.py
+++ main.py
@@ -1,80 +1,58 @@
 import base64
-import httplib2
-import oauth2
 import random
 from xml.etree import ElementTree as etree
-from urllib.parse import urlencode
 import json
 import os
+import requests
+from requests_oauthlib import OAuth1
+import time
 
 CONSUMER_KEY = os.getenv("CONSUMER_KEY")
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
 ACCESS_KEY = os.getenv("ACCESS_KEY")
 ACCESS_SECRET = os.getenv("ACCESS_SECRET")
+FLICKR_API_KEY = os.getenv("FLICKR_API_KEY")
 
 
-def flickr_query_to_get_list_of_pictures(request_h):
-    """
-    function to get list of photos
-    :param request_h: http stream
-    :return: list of bytestrings for each photo
-    """
+def flickr_get_photos_list():
     payload = {'method': 'flickr.people.getPublicPhotos',
-               'api_key': '1a19ecf3ddf46b3323bca01dc8afbd81',
+               'api_key': FLICKR_API_KEY,
                'user_id': '76163487@N05'
                }
-    resp, content = request_h.request('https://api.flickr.com/services/rest/?&{}'.format(urlencode(payload)), "POST")
-    xml = etree.fromstring(content.decode('utf-8'))
-    xml = xml[0]  # get child of root (list of <photo> tags)
-    photos = [{'id': tag.attrib['id'], 'secret': tag.attrib['secret'],
-               "farm": tag.attrib["farm"], "server": tag.attrib["server"]} for tag in xml]
-    return photos
+    photos_list_response = requests.post('https://api.flickr.com/services/rest', data=payload)
+    return [tag.attrib for tag in etree.fromstring(photos_list_response.text)[0]]
 
 
-def flickr_query2(request_h2, photo_index):
-    """
-    function to load photo from server
-    :rtype: object
-    """
-    resp2, content2 = request_h2.request('https://farm{}.staticflickr.com/{}/{}_{}.jpg'.format(
-        photo_index["farm"], photo_index["server"], photo_index['id'], photo_index["secret"]))
-    return content2
+def flickr_get_photo(photo_attributes):
+    flickr_binary_file = requests.get('https://farm{}.staticflickr.com/{}/{}_{}.jpg'.format(
+        photo_attributes["farm"], photo_attributes["server"], photo_attributes['id'], photo_attributes["secret"]))
+    return {"source": flickr_binary_file.content, "name": photo_attributes["title"]}
 
 
-def twitter_query(media_id):
-    # return 1
-    consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
-    access_token = oauth2.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
-    client = oauth2.Client(consumer, access_token)
+def upload_picture_to_twitter(pic):
+    endpoint = "https://upload.twitter.com/1.1/media/upload.json"
+    auth = OAuth1(CONSUMER_KEY, client_secret=CONSUMER_SECRET, resource_owner_key=ACCESS_KEY,
+                  resource_owner_secret=ACCESS_SECRET, signature_type='body')
+    params = {'name': pic["name"], 'media_data': base64.b64encode(pic["source"])}
+    content = requests.post(endpoint, params, auth=auth)
+    return {"id": json.loads(content.text)['media_id'], "name": pic["name"]}
+
+
+def post_to_twitter(text_to_tweet, id_of_photo):
+    auth = OAuth1(CONSUMER_KEY, client_secret=CONSUMER_SECRET, resource_owner_key=ACCESS_KEY,
+                  resource_owner_secret=ACCESS_SECRET, signature_type='body')
     endpoint = "https://api.twitter.com/1.1/statuses/update.json"
-    params = urlencode({'status': 'Hello from Python + Heroku',
-                        'media_ids': media_id})
-    r, c = client.request(endpoint, "POST", params)
+    params = {'status': text_to_tweet, 'media_ids': id_of_photo}
+    c = requests.post(endpoint, params, auth=auth)
     print(c)
 
 
-def upload_picture_to_twitter(bin_pic):
-    consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
-    access_token = oauth2.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
-    client = oauth2.Client(consumer, access_token)
-    endpoint = "https://upload.twitter.com/1.1/media/upload.json"
-    b64_pic = base64.b64encode(bin_pic)
-    params = urlencode({'name': 'picture',
-                        'media_data': b64_pic})
-    status, content = client.request(endpoint, "POST", params,
-                                     urlencode({'Content-Type': 'multipart/form-data'}))
-    return json.loads(content)['media_id']
-
-
 if __name__ == "__main__":
-    h = httplib2.Http(".cache")
-    pictures_list = flickr_query_to_get_list_of_pictures(h)
-    indexes = random.sample(range(0, len(pictures_list)), len(pictures_list))
-    pic = flickr_query2(h, pictures_list[0])
-    pic_id = upload_picture_to_twitter(pic)
-    twitter_query(pic_id)
-    '''for index in indexes:
-        pic = flickr_query2(h, pictures_list[index])
-        with open("pic{}.jpg".format(index), "wb") as file:
-            file.write(pic)
-    '''
+    will_be_uploaded_N_photos = 10
+    while (will_be_uploaded_N_photos>0):
+        pictures_list = flickr_get_photos_list()
+        random_index = random.choice(range(0, len(pictures_list)))
+        photo_flickr = flickr_get_photo(pictures_list[random_index])
+        photo_twitter = upload_picture_to_twitter(photo_flickr)
+        post_to_twitter(photo_twitter["name"], photo_twitter["id"])
+        time.sleep(30)
