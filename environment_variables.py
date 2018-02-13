4637757b842c66f3e767cf5aba632c25e213cf6b
decobots <deco-bots@yandex.ru>
2018-02-13 12:13:57 +0300

    added flickr key #7

--- environment_variables.py
+++ environment_variables.py
@@ -4,3 +4,4 @@ CONSUMER_KEY = os.getenv("CONSUMER_KEY")  # twitter
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")  # twitter
 ACCESS_KEY = os.getenv("ACCESS_KEY")  # twitter
 ACCESS_SECRET = os.getenv("ACCESS_SECRET")  # twitter
+FLICKR_API_KEY = os.getenv("FLICKR_API_KEY")  # flickr
