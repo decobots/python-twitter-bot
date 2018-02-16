6081c93e24f00a154db094b175b71e6477327551
decobots <deco-bots@yandex.ru>
2018-02-16 14:01:29 +0300

    added classes Flickr and request

--- request.py
+++ request.py
@@ -1,9 +1,19 @@
 import requests
+from xml.etree import ElementTree
 
 
-def myrequest(method_type, url, **kwargs):
-    return requests.request(method=method_type,
-                            url=url,
-                            params=kwargs.get("params", ""),
-                            data=kwargs.get("data", ""),
-                            auth=kwargs.get("auth", ""))
+def request(method_type, url, **kwargs):
+    result = requests.request(method=method_type,
+                              url=url,
+                              params=kwargs.get("params", None),
+                              data=kwargs.get("data", None),
+                              auth=kwargs.get("auth", None))
+    try:
+        response_tag = ElementTree.fromstring(result.content)
+        if response_tag.attrib["stat"] == 'fail':
+            raise ValueError(result.text)
+    finally:
+        if result.status_code != "200" and result.status_code != 200:
+            raise ValueError(result.text)
+    return result
+
