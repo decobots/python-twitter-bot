6dfa2ff4437d2f19d64d86050d06da757dfb3898
decobots <deco-bots@yandex.ru>
2018-02-23 14:39:57 +0300

    tests updated

--- test_flickr.py
+++ test_flickr.py
@@ -4,17 +4,14 @@ from flickr import Flickr
 from unittest import mock
 
 
-def get_photos_list_correct_mock_request(*args, **kwargs):
-    m = mock.MagicMock()
-    m.text = '<photos><photo id="2636" secret="a123456" server="2" title="test_04" /></photos>'
-    return m
-
-
 def test_flickr_get_photos_list_correct():
     """
     check that returned value is list and attributes farm, server, id, secret exist for each list item
     """
-    flickr = Flickr(get_photos_list_correct_mock_request)
+    m = mock.MagicMock()
+    m.return_value = mock.MagicMock()
+    m.return_value.text = '<photos><photo id="2636" secret="a123456" server="2" title="test_04" /></photos>'
+    flickr = Flickr(m)
     flickr_get_photos_result = flickr.get_photos()
     assert isinstance(flickr_get_photos_result, list)
     for photo in flickr_get_photos_result:
@@ -25,32 +22,17 @@ def test_flickr_get_photos_list_correct():
         assert photo["secret"] is not None
 
 
-def get_photo_correct_mock_request(*args, **kwargs):
-    m = mock.MagicMock()
-    m.content = b"/ff/ff"
-    return m
-
-
 def test_flickr_get_photo_correct():
     """
     check that returned value exist and have types bytes and string
     """
-    flickr = Flickr(get_photo_correct_mock_request)
+    m = mock.MagicMock()
+    m.return_value = mock.MagicMock()
+    m.return_value.content = b"/ff/ff"
+    flickr = Flickr(m)
     test_flickr_get_photo_result_binary, test_flickr_get_photo_result_name = flickr.get_photo(
         {"farm": "5", "server": "4504", "id": "24003882568", "secret": "ca14f88bec", "title": "test"})
     assert test_flickr_get_photo_result_binary is not None
     assert test_flickr_get_photo_result_name is not None
     assert isinstance(test_flickr_get_photo_result_binary, bytes)
     assert isinstance(test_flickr_get_photo_result_name, str)
-
-
-def get_photo_incorrect_mock_request(*args, **kwargs):
-    raise ValueError
-
-
-def test_flickr_get_photo_incorrect():
-    flickr = Flickr(get_photo_incorrect_mock_request)
-    with pytest.raises(ValueError):
-        flickr.get_photo(
-            {"farm": "5", "server": "450904", "id": "24003882568", "secret": "ca14f88bec", "title": "test"})
-        # invalid server
--- test_twitter.py
+++ test_twitter.py
@@ -1,8 +1,9 @@
+import json
+from unittest import mock
+
 import pytest
 
 from twitter import Twitter
-from unittest import mock
-import base64
 
 
 @pytest.fixture
@@ -11,17 +12,11 @@ def local_picture():
         return f.read()
 
 
-def upload_photo_mock_request(*args, **kwargs):
-    m = mock.MagicMock()
-    if kwargs['payload']['media_data'] == base64.b64encode(local_picture()):
-        m.text = '''{"media_id": 710511363345354753}'''
-    else:  # actually not used
-        m.text = '{"errors":[{"code":215,"message":"Bad Authentication data."}]}'
-    return m
-
-
 def test_upload_photo_correct(local_picture):
-    twitter = Twitter(upload_photo_mock_request)
+    m = mock.MagicMock()
+    m.return_value = mock.MagicMock()
+    m.return_value.text = json.dumps({"media_id": 710511363345354753})
+    twitter = Twitter(m)
     returned_id, returned_name = twitter.upload_photo(name="Test_name", data=local_picture)
     assert returned_id is not None
     assert returned_name is not None
@@ -32,35 +27,29 @@ def test_upload_photo_correct(local_picture):
 
 
 def test_upload_photo_incorrect():
-    twitter = Twitter(upload_photo_mock_request)
+    twitter = Twitter(mock.MagicMock())
     not_picture = "string"
     with pytest.raises(TypeError):  # raised by base64
         twitter.upload_photo(name="Test_name", data=not_picture)
 
 
-def create_post_mock_request(*args, **kwargs):
-    m = mock.MagicMock()
-    m.content = b'''{"id": 243145735212777472}'''
-    return m
-
-
 def test_create_post_correct():
-    twitter = Twitter(create_post_mock_request)
+    m = mock.MagicMock()
+    m.return_value = mock.MagicMock
+    m.return_value.content = json.dumps({"id": 243145735212777472})
+    twitter = Twitter(m)
     result = twitter.create_post(status="Test_status", id_of_photo=1234567)
     assert isinstance(result, int)
 
 
-def get_users_posts_correct_mock_request(*args, **kwargs):
-    m = mock.MagicMock()
-    m.content = b'''[
-                    {"id": 850007368138018817},
-                    {"id": 848930551989915648}
-                ]'''
-    return m
-
-
 def test_get_users_posts_correct():
-    twitter = Twitter(get_users_posts_correct_mock_request)
+    m = mock.MagicMock()
+    m.return_value = mock.MagicMock
+    m.return_value.content = json.dumps([
+        {"id": 850007368138018817},
+        {"id": 848930551989915648}
+    ])
+    twitter = Twitter(m)
     number_of_posts = 2
     result = twitter.get_users_posts(number_of_posts)
     assert len(result) == number_of_posts
