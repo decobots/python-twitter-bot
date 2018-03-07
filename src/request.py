b837403ae7d12ed5dd0e5b572aa66f659bc414e8
decobots <deco-bots@yandex.ru>
2018-03-07 17:45:43 +0300

    logging in request improved

--- src/request.py
+++ src/request.py
@@ -1,5 +1,7 @@
+import json
 import logging
 from xml.etree import ElementTree
+from src.logger import init_logging
 
 import requests
 
@@ -14,25 +16,31 @@ def request(method_type: str, url: str, **kwargs):
         params = kwargs.get("payload", None)
     else:
         data = kwargs.get("payload", None)
-    result = requests.request(method=method_type,
-                              url=url,
-                              params=params,
-                              data=data,
-                              auth=kwargs.get("auth", None))
-    if result.status_code != 200:
-        log.exception(f'response status code !=200 (response status code {result.status_code})')
-        raise ValueError("response status code !=200 ", result.text)
-    # TODO move parsing json and xml to request
-    try:  # try pars as xml, in other case consider that it is json or bytes
-        response_tag = ElementTree.fromstring(result.text)
-        log.debug(f'trying to parse as xml')
-        if response_tag.attrib["stat"] == 'fail':
+    response = requests.request(method=method_type,
+                                url=url,
+                                params=params,
+                                data=data,
+                                auth=kwargs.get("auth", None))
+    if response.status_code != 200:
+        log.exception(f'response status code !=200 (response status code {response.status_code})')
+        raise ValueError("response status code !=200 ", response.text)
+    try:  # try if response xml, in other case consider that it is json or bytes
+        result = ElementTree.fromstring(response.text)
+        if result.attrib["stat"] == 'fail':
             log.exception(f'server returned fail status in xml')
-            raise ValueError(result.text)
-        log.debug(f'parsed as xml')
+            raise ValueError(response.text)
+        log.debug(f'server returned xml')
     except ElementTree.ParseError:  # consider that content not xml
-        if isinstance(result.content, bytes):
-            log.debug("bytes data received")
-        else:
-            log.debug(f'json data received')
-    return result
+        try:  # try if response json, in other case consider that it is bytes
+            response.json()
+            log.debug(f'server returned json')
+        except ValueError:  # consider that content not json
+            response.content
+            log.debug("server returned bytes data")
+
+    return response
+
+
+if __name__ == '__main__':
+    init_logging("test_log.log")
+    request(method_type="GET", url="https://farm4.staticflickr.com/3856/14848963128_9e4e83e446.jpg")
