9590166433b89ced00e29c850ca12952c1e902c8
decobots <deco-bots@yandex.ru>
2018-03-16 19:34:28 +0300

    improve-tests

--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -28,51 +28,51 @@ def teardown_function(func):
     log_func_name_ended(func)
 
 
-def test_upload_photo_correct(photo, mock_requester, mock_db):
+def test_upload_photo_correct(photo, mock_requester, mock_table):
     mock_requester.request_json.return_value = {"media_id": 710511363345354753}
-    twitter = Twitter(requester=mock_requester, table=mock_db)
+    twitter = Twitter(requester=mock_requester, table=mock_table)
     result = twitter.upload_photo(photo=photo)
     assert isinstance(result, Photo)
     assert result.id_twitter == 710511363345354753
 
 
-def test_upload_photo_incorrect(photo, mock_requester, mock_db):
-    twitter = Twitter(requester=mock_requester, table=mock_db)
+def test_upload_photo_incorrect(photo, mock_requester, mock_table):
+    twitter = Twitter(requester=mock_requester, table=mock_table)
     photo.data = "string"
     with pytest.raises(TypeError):  # raised by base64
         twitter.upload_photo(photo=photo)
 
 
-def test_create_post_text_and_photo_correct(photo, mock_requester, mock_db):
+def test_create_post_text_and_photo_correct(photo, mock_requester, mock_table):
     mock_requester.request_json.return_value = {"id": 243145735212777472}
-    mock_db.post_photo = mock.MagicMock()
-    twitter = Twitter(requester=mock_requester, table=mock_db)
+    mock_table.post_photo = mock.MagicMock()
+    twitter = Twitter(requester=mock_requester, table=mock_table)
     result = twitter.create_post(status="Test_status", photo=photo)
-    assert result == 243145735212777472
+    assert result == '243145735212777472'
 
 
-def test_create_post_text_only_correct(mock_requester, mock_db):
+def test_create_post_text_only_correct(mock_requester, mock_table):
     mock_requester.request_json.return_value = {"id": 243145735212777472}
-    mock_db.post_photo = mock.MagicMock()
-    twitter = Twitter(requester=mock_requester, table=mock_db)
+    mock_table.post_photo = mock.MagicMock()
+    twitter = Twitter(requester=mock_requester, table=mock_table)
     result = twitter.create_post(status="Test_status")
-    assert result == 243145735212777472
+    assert result == '243145735212777472'
 
 
-def test_create_post_photo_only_correct(photo, mock_requester, mock_db):
+def test_create_post_photo_only_correct(photo, mock_requester, mock_table):
     mock_requester.request_json.return_value = {"id": 243145735212777472}
-    mock_db.post_photo = mock.MagicMock()
-    twitter = Twitter(requester=mock_requester, table=mock_db)
+    mock_table.post_photo = mock.MagicMock()
+    twitter = Twitter(requester=mock_requester, table=mock_table)
     result = twitter.create_post(photo=photo)
-    assert result == 243145735212777472
+    assert result == '243145735212777472'
 
 
-def test_get_users_posts_correct(mock_requester, mock_db):
+def test_get_users_posts_correct(mock_requester, mock_table):
     mock_requester.request_json.return_value = [
         {"id": 850007368138018817},
         {"id": 848930551989915648}
     ]
-    twitter = Twitter(requester=mock_requester, table=mock_db)
+    twitter = Twitter(requester=mock_requester, table=mock_table)
     number_of_posts = 2
     result = twitter.get_user_posts(number_of_posts)
     assert len(result) == number_of_posts
@@ -80,8 +80,8 @@ def test_get_users_posts_correct(mock_requester, mock_db):
     assert result[1] == 848930551989915648
 
 
-def test_delete_tweet_by_id_correct(mock_requester, mock_db):
-    mock_db.delete_photo_from_twitter = mock.MagicMock()
-    mock_db.delete_photo_from_twitter.return_value = 0
-    twitter = Twitter(requester=mock_requester, table=mock_db)
-    twitter._delete_tweet_by_id(tweet_id=123456)
+def test_delete_tweet_by_id_correct(mock_requester, mock_table):
+    mock_table.delete_photo_from_twitter = mock.MagicMock()
+    twitter = Twitter(requester=mock_requester, table=mock_table)
+    twitter._delete_tweet_by_id(tweet_id='123456')
+
--- tests/test_twitter_end_to_end.py
+++ tests/test_twitter_end_to_end.py
@@ -31,7 +31,7 @@ def setup_module():
     posts = twitter.get_user_posts(100)
     for post in posts:
         twitter._delete_tweet_by_id(post)
-    for n in range(0, 13):
+    for n in range(0, 3):
         twitter.create_post(status=n)
 
 
@@ -50,6 +50,7 @@ def teardown_module():
 @pytest.mark.end_to_end
 def test_upload_photo_correct(photo, empty_table):
     twitter = Twitter(table=empty_table)
+    empty_table.add_photos({photo.id_flickr:photo})
     result = twitter.upload_photo(photo)
     assert isinstance(result, Photo)
     assert isinstance(result.id_twitter, int)
@@ -67,18 +68,20 @@ def test_upload_photo_incorrect(photo, empty_table):
 @pytest.mark.end_to_end
 def test_create_post_correct(photo, empty_table):
     twitter = Twitter(table=empty_table)
+    empty_table.add_photos({photo.id_flickr:photo})
+    twitter.upload_photo(photo)
     result = twitter.create_post(status=photo.title, photo=photo)
-    assert isinstance(result, int)
+    assert isinstance(result, str)
     result = twitter.create_post(status="test")
-    assert isinstance(result, int)
+    assert isinstance(result, str)
     result = twitter.create_post(photo=photo)
-    assert isinstance(result, int)
+    assert isinstance(result, str)
 
 
 @pytest.mark.end_to_end
 def test_get_users_posts_correct(empty_table):
     twitter = Twitter(table=empty_table)
-    number_of_posts = 10
+    number_of_posts = 3
     result = twitter.get_user_posts(number_of_posts)
     assert len(result) == number_of_posts
     for item in result:
@@ -90,3 +93,17 @@ def test_delete_tweet_by_id_no_photo_correct(empty_table):
     twitter = Twitter(table=empty_table)
     tw_id = twitter.create_post(f"test{random.random()}")
     twitter._delete_tweet_by_id(tweet_id=tw_id)
+
+
+@pytest.mark.end_to_end
+def test_delete_tweet_by_id_with_photo_correct(empty_table, photo):
+    twitter = Twitter(table=empty_table)
+    empty_table.add_photos({photo.id_flickr: photo})
+    pic = twitter.upload_photo(photo)
+    tw_id = twitter.create_post(status=f"test{random.random()}", photo=pic)
+    twitter._delete_tweet_by_id(tweet_id=tw_id)
+    posted = empty_table.db.select(
+        query="SELECT * FROM {} WHERE id=%s",
+        identifiers=[TABLE_NAME],
+        arguments=(photo.id_flickr,))
+    assert posted == [(photo.id_flickr, False, None)]
