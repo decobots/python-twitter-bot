03de4b22e06472068625385dd73806328142d29c
decobots <deco-bots@yandex.ru>
2018-03-22 14:56:35 +0300

    added svg maker

--- src/svgmaker.py
+++ src/svgmaker.py
@@ -4,28 +4,34 @@ from typing import List
 
 import svgwrite
 
-from src.color_table import Color, ColorTable
+from src.color_table import Color, ColorTable, colors_delta
 
 DATA_DEFAULT_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
 W = 140
 H = 40
 
 
-def create_svg(color_groups: List[List['Color']]):
-    with open(os.path.join(DATA_DEFAULT_PATH, "test.svg"), mode='w') as f:
+def create_svg(color_groups: List[List['colors_delta']], filename: str = "test.svg"):
+    with open(os.path.join(DATA_DEFAULT_PATH, filename), mode='w') as f:
         dwg = svgwrite.Drawing(size=(f'{W * len(color_groups[0])}px',
                                      f'{H * len(color_groups)}px'
                                      ))
         for group, y in zip(color_groups, range(0, len(color_groups) * H, H)):
             for x, color in enumerate(group):
-                rect = dwg.rect(insert=(x * W, y), size=(W, H), fill=color.hex)
+                rect = dwg.rect(insert=(x * W, y), size=(W, H), fill=color.color.hex)
                 dwg.add(rect)
-                text = dwg.text([int(v) for v in color.lab_values],
+                text = dwg.text([int(v) for v in color.color.rgb_values],
                                 insert=(x * W, y),
                                 dy=[H / 2],
                                 style=f'font-size:{H/2.2}px'
                                 )
                 dwg.add(text)
+                text2 = dwg.text("{0:.2f} {1}".format(color.delta,color.color.name),
+                                 insert=(x * W, y),
+                                 dy=[H / 2 + H / 3],
+                                 style=f'font-size:{H/2.2}px'
+                                 )
+                dwg.add(text2)
         dwg.write(f)
 
 
